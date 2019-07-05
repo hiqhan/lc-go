5fdb582eddccc54af63f511a807d35953ecc1668
hiqhan <qiuhan.h@gmail.com>
2019-07-05 23:33:22 +0800

    modify solution

--- add-two-number/add_two_number.go
+++ add-two-number/add_two_number.go
@@ -1,64 +1,75 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
+func walk(ln *ListNode, ch chan int) {
+	var doWalk func(ln *ListNode)
+	doWalk = func(ln *ListNode) {
+		if ln == nil {
+			return
+		}
+		ch <- ln.Val
+		doWalk(ln.Next)
+	}
+	doWalk(ln)
+	close(ch)
+}
+
+func Walk(ln *ListNode) string {
+	ch := make(chan int)
+	go walk(ln, ch)
+	var s string
+	for {
+		i, ok := <-ch
+		if ok {
+			s += strconv.Itoa(i)
+		} else {
+			break
+		}
+	}
+	return s
+}
+
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	var l3, present *ListNode
+	dummyHead := new(ListNode)
+	p, q, curr := l1, l2, dummyHead
 	carry := 0
-	for ; l1 != nil && l2 != nil; l1, l2 = l1.Next, l2.Next {
-		n := l1.Val + l2.Val + carry
-		if n >= 10 {
-			carry = 1
-			n -= 10
-		} else {
-			carry = 0
+	for p != nil || q != nil {
+		var x, y int
+		if x = 0; p != nil {
+			x = p.Val
 		}
-		node := ListNode{n, nil}
-		if present == nil {
-			l3 = &node
-		} else {
-			present.Next = &node
+		if y = 0; q != nil {
+			y = q.Val
 		}
-		present = &node
-	}
-	for ; l1 != nil; l1 = l1.Next {
-		n := l1.Val + carry
-		if n >= 10 {
-			carry = 1
-			n -= 10
-		} else {
-			carry = 0
+		sum := x + y + carry
+		carry = sum / 10
+		curr.Next = &ListNode{sum % 10, nil}
+		curr = curr.Next
+		if p != nil {
+			p = p.Next
 		}
-		node := ListNode{n, nil}
-		present.Next = &node
-		present = &node
-	}
-	for ; l2 != nil; l2 = l2.Next {
-		n := l2.Val + carry
-		if n >= 10 {
-			carry = 1
-			n -= 10
-		} else {
-			carry = 0
+		if q != nil {
+			q = q.Next
 		}
-		node := ListNode{n, nil}
-		present.Next = &node
-		present = &node
 	}
 	if carry > 0 {
-		node := ListNode{carry, nil}
-		present.Next = &node
-		present = &node
+		curr.Next = &ListNode{1, nil}
 	}
-	return l3
+	return dummyHead
 }
 
 func main() {
 	a := ListNode{9, nil}
 	b := ListNode{9, nil}
 	c := ListNode{1, nil}
-	addTwoNumbers(&a, &c)
+	fmt.Println(Walk(addTwoNumbers(&a, &c)))
 }
