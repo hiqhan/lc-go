0936aedf2babf01273c54ceb303e244355d64160
hiqhan <qiuhan.h@gmail.com>
2019-07-20 19:25:49 +0800

    feat: update p37 solution

--- p37_sudoku.go
+++ p37_sudoku.go
@@ -1,35 +1,32 @@
-package leetcode
+package main
 
 import "fmt"
 
-const N int = 9
-const Empty byte = '.'
+const n int = 9
+const e byte = '.'
 
 func solveSudoku(board [][]byte) bool {
 	row, col, ok := findEmptyCell(board)
 	if !ok {
-		for _, l := range board {
-			fmt.Println(l)
-		}
 		return true
 	}
 
 	for i := 1; i <= 9; i++ {
-		if isSafe(board, row, col, byte(i)) {
-			board[row][col] = byte(i)
+		if isSafe(board, row, col, byte(48+i)) {
+			board[row][col] = byte(48 + i)
 			if solveSudoku(board) {
 				return true
 			}
-			board[row][col] = Empty
+			board[row][col] = e
 		}
 	}
 	return false
 }
 
 func findEmptyCell(board [][]byte) (row, col int, ok bool) {
-	for row := 0; row < N; row++ {
-		for col := 0; col < N; col++ {
-			if board[row][col] == Empty {
+	for row := 0; row < n; row++ {
+		for col := 0; col < n; col++ {
+			if board[row][col] == e {
 				return row, col, true
 			}
 		}
@@ -38,11 +35,14 @@ func findEmptyCell(board [][]byte) (row, col int, ok bool) {
 }
 
 func isSafe(board [][]byte, row, col int, num byte) bool {
-	return isRowSafe(board, row, num) && isColSafe(board, col, num) && isBoxSafe(board, row, col, num)
+	return board[row][col] == e &&
+		isRowSafe(board, row, num) &&
+		isColSafe(board, col, num) &&
+		isBoxSafe(board, row, col, num)
 }
 
 func isRowSafe(board [][]byte, row int, num byte) bool {
-	for i := 0; i < N; i++ {
+	for i := 0; i < n; i++ {
 		if board[row][i] == num {
 			return false
 		}
@@ -51,7 +51,7 @@ func isRowSafe(board [][]byte, row int, num byte) bool {
 }
 
 func isColSafe(board [][]byte, col int, num byte) bool {
-	for i := 0; i < N; i++ {
+	for i := 0; i < n; i++ {
 		if board[i][col] == num {
 			return false
 		}
@@ -60,7 +60,7 @@ func isColSafe(board [][]byte, col int, num byte) bool {
 }
 
 func isBoxSafe(board [][]byte, row, col int, num byte) bool {
-	startRow, startCol := row/3, col/3
+	startRow, startCol := row-row%3, col-col%3
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 3; j++ {
 			if board[startRow+i][startCol+j] == num {
@@ -69,4 +69,27 @@ func isBoxSafe(board [][]byte, row, col int, num byte) bool {
 		}
 	}
 	return true
+}
+
+func printBoard(b [][]byte) {
+	for _, l := range b {
+		fmt.Printf("%c\n", l)
+	}
+}
+
+func main() {
+	board := [][]byte{
+		[]byte(string("53..7....")),
+		[]byte(string("6..195...")),
+		[]byte(string(".98....6.")),
+		[]byte(string("8...6...3")),
+		[]byte(string("4..8.3..1")),
+		[]byte(string("7...2...6")),
+		[]byte(string(".6....28.")),
+		[]byte(string("...419..5")),
+		[]byte(string("....8..79")),
+	}
+	if solveSudoku(board) {
+		printBoard(board)
+	}
 }
--- p37_sudoku_test.go
+++ p37_sudoku_test.go
@@ -12,6 +12,32 @@ func Test_solveSudoku(t *testing.T) {
 		want bool
 	}{
 		// TODO: Add test cases.
+		{
+			name: "frist test",
+			args: args{
+				[][]byte{
+					// {5, 3, e, e, 7, e, e, e, e},
+					// {6, e, e, 1, 9, 5, e, e, e},
+					// {e, 9, 8, e, e, e, e, 6, e},
+					// {8, e, e, e, 6, e, e, e, 3},
+					// {4, e, e, 8, e, 3, e, e, 1},
+					// {7, e, e, e, 2, e, e, e, 6},
+					// {e, 6, e, e, e, e, 2, 8, e},
+					// {e, e, e, 4, 1, 9, e, e, 5},
+					// {e, e, e, e, 8, e, e, 7, 9},
+					[]byte(string("53..7....")),
+					[]byte(string("6..195...")),
+					[]byte(string(".98....6.")),
+					[]byte(string("8...6...3")),
+					[]byte(string("4..8.3..1")),
+					[]byte(string("7...2...6")),
+					[]byte(string(".6....28.")),
+					[]byte(string("...419..5")),
+					[]byte(string("....8..79")),
+				},
+			},
+			want: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
